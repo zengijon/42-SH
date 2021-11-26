9a7a84b6f0fe725c22cc6d3bd061c0f7a1a2b0f8
robin <robin.jannes@epita.fr>
2021-11-26 18:35:44 +0100

    test

---
+++ cmake-build-debug/CMakeFiles/42sheee.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "42sheee"
+  "42sheee.pdb"
+  "CMakeFiles/42sheee.dir/src/lexer/lexer.c.o"
+  "CMakeFiles/42sheee.dir/src/lexer/test_lexer.c.o"
+  "CMakeFiles/42sheee.dir/src/lexer/token.c.o"
+  "CMakeFiles/42sheee.dir/src/lexer/utils.c.o"
+  "CMakeFiles/42sheee.dir/src/memory/free_list.c.o"
+  "CMakeFiles/42sheee.dir/src/memory/hmalloc.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/42sheee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
