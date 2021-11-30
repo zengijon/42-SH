351b09365a3a0c5a192dd8e6bf20ed85baef9bc4
clousty <armand.mounsi@gmail.com>
2021-11-30 18:14:11 +0100

    solving conflict

---
+++ src/exec_builtins/exec_cmds.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//char *builtins[] = {"echo", "cd", "export", "continue", "break", "pitfall", "dot", "unset"};
+int exec_builtins(char *cmd, int nb_params, char **parameters);
+int exec_cmds(char *cmd, int nb_params, char **parameters);
