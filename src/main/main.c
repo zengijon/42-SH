1a2849245fa2288a774ec704141a67927285de8d
yohann.ait-seddik <yohann.ait-seddik@epita.fr>
2021-12-01 18:42:55 +0100

    Repair the testsuite and backslash Also add '!' and 'until' token

--- src/main/main.c
+++ src/main/main.c
@@ -15,7 +15,7 @@ struct free_list *list_malloc = NULL;
 int main(int argc, char **argv)
 {
   if(argc == 1)
-    return 0;
+      return 0;
   static const struct option longOpts[] = {
         { "pretty_print", no_argument, NULL, 'p' },
         { "cmd", required_argument, NULL, 'c' },
