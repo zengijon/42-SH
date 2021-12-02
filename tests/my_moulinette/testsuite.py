850e5e803d65a80bd2a23630593f8af28fe14eca
Addam Ouedghiri <addam.ouedghiri-benaaddach@epita.fr>
2021-12-02 01:49:05 +0100

    TestSuite Presque Total OK

--- tests/my_moulinette/testsuite.py
+++ tests/my_moulinette/testsuite.py
@@ -3,11 +3,28 @@ from pathlib import Path
 from difflib import unified_diff
 
 import subprocess as sp
+from typing import cast
 import termcolor
 import yaml
 
-TEST_OK = f"\n[ {termcolor.colored('OK', 'green')} ]"
-TEST_KO = f"\n[ {termcolor.colored('KO', 'red')} ]"
+TEST_OK = f"[ {termcolor.colored('OK', 'green')} ]"
+TEST_KO = f"[ {termcolor.colored('KO', 'red')} ]"
+RED_DOT = f"{termcolor.colored('*', 'red')}"
+
+ACTUAL = f"{termcolor.colored('---ACTUAL---', 'blue')}"
+EXPECTED = f"{termcolor.colored('---EXPECTED---', 'magenta')}"
+
+def check_stderr(stderr_output: str):
+    length = len(stderr_output)
+    if (length < 6):
+        return False
+    to_compare = ""
+    compared = "42sh:"
+    i = 0
+    while (i < 6):
+        to_compare += stderr_output[i]
+        i += 1
+    return to_compare == compared
 
 
 def my_diff(expected: str, actual: str):
@@ -19,14 +36,39 @@ def my_diff(expected: str, actual: str):
 def running(shell: str, stdin: str) -> sp.CompletedProcess:
     return sp.run([shell], input=stdin, capture_output=True, text=True)
 
+def running_process(shell: str, stdin: str) -> sp.CompletedProcess:
+    return sp.run([shell, stdin], capture_output=True, text=True)
 
-def check_output(expected: sp.CompletedProcess, actual: sp.CompletedProcess):
-    assert expected.returncode == actual.returncode, \
-        f"Exit with return code -> {actual.returncode}, and -> {expected.returncode} was expected"
-    assert expected.stdout == actual.stdout, \
-        f"Stdout is not the same\n{my_diff(expected.stdout, actual.stdout)}"
-    assert expected.stderr == actual.stderr, \
-        f"Stderr is not the same\n{my_diff(expected.stderr, actual.stderr)}"
+def my_check_output(expected: sp.CompletedProcess, actual: sp.CompletedProcess, flag: int, name: str, input: str):
+    if (expected.returncode != actual.returncode):
+        flag += 1
+    if (expected.stdout != actual.stdout):
+        flag += 1
+    if (expected.stderr != actual.stderr):
+        flag += 1
+    
+    if (flag == 0):
+        print(f"{TEST_OK} {name}")
+    else:
+        print(f"{TEST_KO} {name}\n")
+        print(f"INPUT: {input}")
+        if (expected.returncode != actual.returncode):
+            print(f"\n{RED_DOT} Exit with return code -> {actual.returncode}, and -> {expected.returncode} was expected")
+        if (expected.stdout != actual.stdout):
+            print(f"\n{RED_DOT} Stdout is not the same\n{ACTUAL}\n{actual.stdout}\n{EXPECTED}\n{expected.stdout}") 
+        if (expected.stderr != ""): #PR VOIR SI UN MSG DERREUR EST ATTENDU
+            if (check_stderr(actual.stderr) == False): #CHECK SI LE MSG DERREUR COMMENCE PAR '42sh:'
+                print(f"\n{RED_DOT} Stderr is not the same\n {ACTUAL}\n{actual.stderr}\n{EXPECTED}\n"
+)
+
+
+#def check_output(expected: sp.CompletedProcess, actual: sp.CompletedProcess):
+#    assert expected.returncode == actual.returncode, \
+ #       f"Exit with return code -> {actual.returncode}, and -> {expected.returncode} was expected"
+  #  assert expected.stdout == actual.stdout, \
+   #     f"Stdout is not the same\n{ACTUAL}\n{actual.stdout}\n{EXPECTED}\n{expected.stdout}"
+    #assert expected.stderr == actual.stderr, \
+     #   f"Stderr is not the same\n {ACTUAL}\n{actual.stderr}\n{EXPECTED}\n"
 
 
 if __name__ == "__main__":
@@ -37,19 +79,29 @@ if __name__ == "__main__":
     path_42sh = arg.binary.absolute()
     print(f"We are testing the following binary -> {path_42sh}")
 
-    with open("tests_def.yml", "r") as our_yaml:
+    with open("script_echo_tests.yml", "r") as our_yaml:
         tests_list = list(yaml.safe_load(our_yaml))
 
+    cat_list = ["ECHO SCRIPT TESTS"]
+    i = 0
+    print(f"\n============ECHO SCRIPT TESTS============")
     for test in tests_list:
+        cat = test["category"]
+        if (cat_list[i] != cat):
+            print(f"\n============{cat}============")
+            cat_list.append(cat)
+            i += 1
+
         our_input = test["input"]
         name = test["name"]
 
         process_dash = running("dash", our_input)
-        process_42sh = running(path_42sh, our_input)
 
-        try:
-            check_output(process_dash, process_42sh)
-        except AssertionError as err:
-            print(f"{TEST_KO} {name}\n{err}")
-        else:
-            print(f"{TEST_OK} {name}")
+        #input_42sh = "-c "
+        #input_42sh = "\"" + our_input + "\""
+        #print(our_input)
+        #res = str(path_42sh) + " " + input_42sh
+        #print(f"\n LE PATH: {path_42sh}, -c <{our_input}>\n")
+        process_42sh = running_process(path_42sh, our_input)
+
+        my_check_output(process_dash, process_42sh, 0, name, our_input)
