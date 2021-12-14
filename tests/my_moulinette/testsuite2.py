92b68850f5458efb10a5917378aea63254df3802
yohann.ait-seddik <yohann.ait-seddik@epita.fr>
2021-12-14 18:53:08 +0100

    Auto stash before merge of "yohann" and "origin/addam_"
    Merge

--- tests/my_moulinette/testsuite2.py
+++ tests/my_moulinette/testsuite2.py
@@ -7,16 +7,11 @@ from typing import cast
 import termcolor
 import yaml
 
-basic_files = ["if_basic_tests.yml", "cmd_var.yml", "cmd_echo_tests.yml"]
+basic_files = ["if_basic_tests.yml", "cmd_var.yml", "cd_tests_basic.yml", "cmd_for_tests.yml", "cmd_pipe.yml", "function_basic.yml", "pipe_basic_tests.yml"]
 error_files = ["if_error_tests.yml", "cmd_error_var.yml"]
 hard_files = []
-<<<<<<< HEAD
-script_files = ["shell_script/cmd_var/arg_basics.sh", "shell_script/cmd_var/nega.sh", "shell_script/cmd_var/IFS_1.sh", 
-                "shell_script/complex_script/pipe_if_for.sh", "shell_script/complex_script/pipe_if_for_2.sh", "shell_script/complex_script/pipe_if_for_3.sh"]
-=======
 script_files = ["shell_script/cmd_var/arg_basics.sh", "shell_script/cmd_var/nega.sh", "shell_script/cmd_var/IFS_1.sh", "shell_script/complex_script/pipe_if_for.sh", 
                 "shell_script/complex_script/pipe_if_for_2.sh", "shell_script/complex_script/pipe_if_for_3.sh", "shell_script/redir/pipe.sh", "shell_script/redir/redir_basics.sh", "shell_script/for/simple_for_1.sh"]
->>>>>>> 788fac083e8f375d8223dd0c1cb283e4344fd3e3
 
 TEST_OK = f"[ {termcolor.colored('OK', 'green')} ]"
 TEST_KO = f"[ {termcolor.colored('KO', 'red')} ]"
