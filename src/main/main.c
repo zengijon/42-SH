447030d7ddf388df0fd4fe4197db6ddf2708f4d9
Jeanne <jennifer.ter@epita.fr>
2021-12-15 20:38:01 +0100

    je crois que ca marche .. enfin j'espere

--- src/main/main.c
+++ src/main/main.c
@@ -11,24 +11,11 @@
 #include "../utils/file2buf.h"
 #include "stdio.h"
 #include "../utils/usefull_fonction.h"
+#include "../utils/for_pwd.h"
 
 struct free_list *list_malloc = NULL;
 
-char *get_path(void)
-{
-    char s[2048] = { 0 };
-    getcwd(s, 2048);
-//    if (s == NULL)
-//        errx(2, "getcwd failed");
-
-    char *res = hcalloc(1, strlen(s) + 5);
-
-    memccpy(res, s, '\0', strlen(s));
-
-    return res;
-}
-
-struct exec_struct *build_exec_struct(int argc, char **argv)
+struct exec_struct *build_exec_struct(int argc, char **argv, int indic)
 {
     char *A_starval = hcalloc(15536, 1);
     struct exec_struct *e_x = hcalloc(1, sizeof(struct exec_struct));
@@ -45,8 +32,8 @@ struct exec_struct *build_exec_struct(int argc, char **argv)
     assign_var("#", my_itoa(argc - optind - 1, hcalloc(1, 8)), e_x);
     assign_var("$", my_itoa(getpid(), hcalloc(1, 8)), e_x);
     assign_var("IFS", "\n", e_x);
-    assign_var("OLDPWD", get_path(),e_x);
-    assign_var("PWD", get_path(), e_x);
+    assign_var("OLDPWD", get_path(indic, argv),e_x);
+    assign_var("PWD", get_path(indic, argv), e_x);
     assign_var("HOME", getenv("HOME"), e_x);
     assign_var("?", "0", e_x);
     return e_x;
@@ -80,7 +67,7 @@ int main(int argc, char **argv)
             return 2;
         }
         char *buffer = stdin2buf();
-        return exec_42sh(buffer, 0, build_exec_struct(argc, argv));
+        return exec_42sh(buffer, 0, build_exec_struct(argc, argv, -1));
     }
     static const struct option longOpts[] = {
         { "pretty_print", no_argument, NULL, 'p' },
@@ -119,5 +106,5 @@ int main(int argc, char **argv)
                                  // reading stdin
     if (!c)
         buffer = file2buf(argv[optind]);
-    return exec_42sh(buffer, pretty_print, build_exec_struct(argc, argv));
+    return exec_42sh(buffer, pretty_print, build_exec_struct(argc, argv, optind));
 }
