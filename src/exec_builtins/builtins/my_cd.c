c57cad8c9978a142da49b9c23ec13a7752927d1b
Jeanne <jennifer.ter@epita.fr>
2021-12-16 01:08:11 +0100

    cd style

--- src/exec_builtins/builtins/my_cd.c
+++ src/exec_builtins/builtins/my_cd.c
@@ -2,16 +2,15 @@
 // Created by jennie on 06/12/2021.
 //
 
-#include "builtins.h"
-
 #include <ctype.h>
 #include <dirent.h>
-#include <err.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 #include "../../memory/hmalloc.h"
+#include "builtins.h"
 
 char *final_path(char *name, char *current_path)
 {
@@ -29,7 +28,7 @@ int is_valid_dir(char *path)
     if (pDir == NULL)
         return 0;
 
-    closedir (pDir);
+    closedir(pDir);
     return 1;
 }
 
@@ -41,7 +40,8 @@ char *find_e_x(struct exec_struct *e_x, char *name)
     return NULL;
 }
 
-const char *get_cd_arg(char *arg, char *current_path, struct exec_struct *e_x, int *need_pwd)
+char *get_cd_arg(char *arg, char *current_path, struct exec_struct *e_x,
+                       int *need_pwd)
 
 {
     if (arg == NULL || strcmp(arg, ";") == 0)
@@ -78,22 +78,69 @@ const char *get_cd_arg(char *arg, char *current_path, struct exec_struct *e_x, i
         return is_valid_dir(path) == 1 ? arg : NULL;
 }
 
-void change_pwd_var(char *current_path, const char *arg_cd, struct exec_struct *e_x)
+int count_par(const char *arg_cd)
 {
-    if (strcmp(arg_cd, ".") == 0 || strcmp(arg_cd, ";") == 0) // on ne change pas de working dir
+    int count = 0;
+    for (int i = 0; arg_cd[i] != '\0'; ++i)
+        if (arg_cd[i] == '/')
+            count++;
+    return count;
+}
+
+char *for_double_pts(int len, char *current_path, char *res) // res deja calloc
+{
+    int i = len - 1;
+    while (i >= 0)
+    {
+        if (current_path[i] == '/')
+        {
+            current_path[i] = '&';
+            break;
+        }
+        i--;
+    }
+    memccpy(res, current_path, '&', len - i - 1);
+    res[len - i - 2] = '/';
+    return res;
+}
+
+char *change_path(char *current_path, const char *arg_cd)
+{
+    char *res = hcalloc(1, strlen(current_path) + strlen(arg_cd) + 25);
+    int len = (int)strlen(current_path);
+    if (strcmp(current_path, "/") == 0)
+        return current_path;
+    if (strcmp(arg_cd, "..") == 0 || strcmp(arg_cd, "../") == 0)
+        res = for_double_pts(len, current_path, res);
+
+    else
+    {
+        ;
+        // si on a beaucoup de ../ ../
+        // si on a ../dir ou ../../ ... /dir
+    }
+    return res;
+}
+
+void change_pwd_var(char *current_path, const char *arg_cd,
+                    struct exec_struct *e_x)
+{
+    if (strcmp(arg_cd, ".") == 0
+        || strcmp(arg_cd, ";") == 0) // on ne change pas de working dir
     {
         assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
     }
 
-    else if (strcmp(arg_cd, "..") == 0 || strcmp(arg_cd, "../") == 0) // on recule de 1
+    else if (strcmp(arg_cd, "..") == 0
+             || strcmp(arg_cd, "../") == 0) // on recule de 1
     {
-        char *s = getenv("PWD");
+        char *s = change_path(current_path, arg_cd);
         assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
         assign_var("PWD", s, e_x);
     }
     else if (arg_cd[0] == '.' && arg_cd[1] == '.') // str de ../
     {
-        char *s = getenv("PWD");
+        char *s = change_path(current_path, arg_cd);
         assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
         assign_var("PWD", s, e_x);
     }
@@ -112,7 +159,7 @@ void change_pwd_var(char *current_path, const char *arg_cd, struct exec_struct *
     else if (strcmp(arg_cd, find_e_x(e_x, "HOME")) == 0)
     {
         char *temp = find_e_x(e_x, "PWD");
-        assign_var("PWD", (char *) arg_cd, e_x);
+        assign_var("PWD", (char *)arg_cd, e_x);
         assign_var("OLDPWD", temp, e_x);
     }
     else
@@ -126,16 +173,23 @@ void change_pwd_var(char *current_path, const char *arg_cd, struct exec_struct *
 
 int my_pwd(struct exec_struct *e_x)
 {
-    printf("%s\n",find_e_x(e_x, "PWD"));
+    printf("%s\n", find_e_x(e_x, "PWD"));
     return 0;
 }
+
+void change_pwd_var2(char *current_path, const char *arg_cd, struct exec_struct *e_x);
+
 
 int my_cd(char **argv, struct exec_struct *e_x)
 {
     int need_pwd = 0;
     char *current_path = find_e_x(e_x, "PWD"); // path avant changement
-
-    const char *arg_cd = get_cd_arg(argv[1], current_path, e_x, &need_pwd); // arg apres cd valid ou pas
+    if (current_path == NULL)
+    {
+        fprintf(stderr, "PWD has not yet been define\n");
+    }
+    char *arg_cd = get_cd_arg(argv[1], current_path, e_x,
+                                    &need_pwd); // arg apres cd valid ou pas
 
     if (arg_cd == NULL)
     {
@@ -143,20 +197,64 @@ int my_cd(char **argv, struct exec_struct *e_x)
         return 2;
     }
 
-    char *temp = final_path((char *) arg_cd, current_path);
-    if (chdir(arg_cd) == -1) // change le working dir
+    //gerer les chemins ABS
+    if (*arg_cd == '/')
     {
-        if (chdir(temp) == -1)
-        {
+        assign_var("OLDPWD", current_path, e_x);
+        assign_var("PWD", arg_cd, e_x);
+    }
+    else
+        change_pwd_var2(current_path, arg_cd, e_x);
+    if (chdir(find_e_x(e_x, "PWD")) == -1) // change le working dir
+    {
             fprintf(stderr, "Chdir failed\n");
             return 2;
-        }
     }
 
-    change_pwd_var(current_path, arg_cd, e_x); // met a jour OLDPWD et PWD
+     // met a jour OLDPWD et PWD
 
     if (need_pwd == 1)
         printf("%s\n", current_path);
 
     return 0;
+}
+
+int poplast(char *newpath)
+{
+    if (*newpath == 0)
+        return 1;
+    int res = poplast(1 + newpath);
+    if (res && *newpath == '/')
+    {
+        *newpath = 0;
+        return 0;
+    }
+    return res;
+}
+
+void change_pwd_var2(char *current_path, const char *arg_cd, struct exec_struct *e_x)
+{
+    char *newpath = hcalloc(10, strlen(current_path));
+    strcpy(newpath, current_path);
+    char *trash = hcalloc(1, strlen(arg_cd));
+    strcpy(trash, arg_cd);
+    char *tmp = NULL;
+    (tmp = strtok(trash, "/"));
+    do {
+        if (strcmp(tmp, ".") == 0)
+            ;
+        else if (strcmp(tmp, "..") == 0)
+        {
+                poplast(newpath);
+        }
+        else
+        {
+            strcat(newpath, "/");
+            strcat(newpath, tmp);
+        }
+    } while ((tmp = strtok(NULL, "/")) != 0);
+    if (strlen(newpath) == 0)
+        strcat(newpath, "/");
+    assign_var("OLDPWD", current_path, e_x);
+    assign_var("PWD", newpath, e_x);
 }
