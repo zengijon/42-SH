b64e7f54205c72ca3a42b5fa295cc946b57cc42e
Jeanne <jennifer.ter@epita.fr>
2021-12-16 01:24:13 +0100

    cd style

--- src/exec_builtins/builtins/my_cd.c
+++ src/exec_builtins/builtins/my_cd.c
@@ -47,6 +47,8 @@ char *get_cd_arg(char *arg, char *current_path, struct exec_struct *e_x,
     if (arg == NULL || strcmp(arg, ";") == 0)
         return find_e_x(e_x, "HOME");
 
+    else if (arg[0] == '~')
+        return arg;
     char *path = final_path(arg, current_path);
 
     if (strcmp(arg, "-") == 0)
@@ -78,98 +80,98 @@ char *get_cd_arg(char *arg, char *current_path, struct exec_struct *e_x,
         return is_valid_dir(path) == 1 ? arg : NULL;
 }
 
-int count_par(const char *arg_cd)
-{
-    int count = 0;
-    for (int i = 0; arg_cd[i] != '\0'; ++i)
-        if (arg_cd[i] == '/')
-            count++;
-    return count;
-}
+//int count_par(const char *arg_cd)
+//{
+//    int count = 0;
+//    for (int i = 0; arg_cd[i] != '\0'; ++i)
+//        if (arg_cd[i] == '/')
+//            count++;
+//    return count;
+//}
+//
+//char *for_double_pts(int len, char *current_path, char *res) // res deja calloc
+//{
+//    int i = len - 1;
+//    while (i >= 0)
+//    {
+//        if (current_path[i] == '/')
+//        {
+//            current_path[i] = '&';
+//            break;
+//        }
+//        i--;
+//    }
+//    memccpy(res, current_path, '&', len - i - 1);
+//    res[len - i - 2] = '/';
+//    return res;
+//}
 
-char *for_double_pts(int len, char *current_path, char *res) // res deja calloc
-{
-    int i = len - 1;
-    while (i >= 0)
-    {
-        if (current_path[i] == '/')
-        {
-            current_path[i] = '&';
-            break;
-        }
-        i--;
-    }
-    memccpy(res, current_path, '&', len - i - 1);
-    res[len - i - 2] = '/';
-    return res;
-}
+//char *change_path(char *current_path, const char *arg_cd)
+//{
+//    char *res = hcalloc(1, strlen(current_path) + strlen(arg_cd) + 25);
+//    int len = (int)strlen(current_path);
+//    if (strcmp(current_path, "/") == 0)
+//        return current_path;
+//    if (strcmp(arg_cd, "..") == 0 || strcmp(arg_cd, "../") == 0)
+//        res = for_double_pts(len, current_path, res);
+//
+//    else
+//    {
+//        ;
+//        // si on a beaucoup de ../ ../
+//        // si on a ../dir ou ../../ ... /dir
+//    }
+//    return res;
+//}
 
-char *change_path(char *current_path, const char *arg_cd)
-{
-    char *res = hcalloc(1, strlen(current_path) + strlen(arg_cd) + 25);
-    int len = (int)strlen(current_path);
-    if (strcmp(current_path, "/") == 0)
-        return current_path;
-    if (strcmp(arg_cd, "..") == 0 || strcmp(arg_cd, "../") == 0)
-        res = for_double_pts(len, current_path, res);
-
-    else
-    {
-        ;
-        // si on a beaucoup de ../ ../
-        // si on a ../dir ou ../../ ... /dir
-    }
-    return res;
-}
-
-void change_pwd_var(char *current_path, const char *arg_cd,
-                    struct exec_struct *e_x)
-{
-    if (strcmp(arg_cd, ".") == 0
-        || strcmp(arg_cd, ";") == 0) // on ne change pas de working dir
-    {
-        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
-    }
-
-    else if (strcmp(arg_cd, "..") == 0
-             || strcmp(arg_cd, "../") == 0) // on recule de 1
-    {
-        char *s = change_path(current_path, arg_cd);
-        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
-        assign_var("PWD", s, e_x);
-    }
-    else if (arg_cd[0] == '.' && arg_cd[1] == '.') // str de ../
-    {
-        char *s = change_path(current_path, arg_cd);
-        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
-        assign_var("PWD", s, e_x);
-    }
-    else if (strcmp(arg_cd, "-") == 0)
-    {
-        char *temp = find_e_x(e_x, "OLDPWD");
-        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
-        assign_var("PWD", temp, e_x);
-    }
-    else if (strcmp(arg_cd, find_e_x(e_x, "OLDPWD")) == 0)
-    {
-        char *temp = find_e_x(e_x, "OLDPWD");
-        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
-        assign_var("PWD", temp, e_x);
-    }
-    else if (strcmp(arg_cd, find_e_x(e_x, "HOME")) == 0)
-    {
-        char *temp = find_e_x(e_x, "PWD");
-        assign_var("PWD", (char *)arg_cd, e_x);
-        assign_var("OLDPWD", temp, e_x);
-    }
-    else
-    {
-        char *temp = find_e_x(e_x, "PWD");
-        char *res = final_path((char *)arg_cd, current_path);
-        assign_var("OLDPWD", temp, e_x);
-        assign_var("PWD", res, e_x);
-    }
-}
+//void change_pwd_var(char *current_path, const char *arg_cd,
+//                    struct exec_struct *e_x)
+//{
+//    if (strcmp(arg_cd, ".") == 0
+//        || strcmp(arg_cd, ";") == 0) // on ne change pas de working dir
+//    {
+//        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
+//    }
+//
+//    else if (strcmp(arg_cd, "..") == 0
+//             || strcmp(arg_cd, "../") == 0) // on recule de 1
+//    {
+//        char *s = change_path(current_path, arg_cd);
+//        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
+//        assign_var("PWD", s, e_x);
+//    }
+//    else if (arg_cd[0] == '.' && arg_cd[1] == '.') // str de ../
+//    {
+//        char *s = change_path(current_path, arg_cd);
+//        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
+//        assign_var("PWD", s, e_x);
+//    }
+//    else if (strcmp(arg_cd, "-") == 0)
+//    {
+//        char *temp = find_e_x(e_x, "OLDPWD");
+//        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
+//        assign_var("PWD", temp, e_x);
+//    }
+//    else if (strcmp(arg_cd, find_e_x(e_x, "OLDPWD")) == 0)
+//    {
+//        char *temp = find_e_x(e_x, "OLDPWD");
+//        assign_var("OLDPWD", find_e_x(e_x, "PWD"), e_x);
+//        assign_var("PWD", temp, e_x);
+//    }
+//    else if (strcmp(arg_cd, find_e_x(e_x, "HOME")) == 0)
+//    {
+//        char *temp = find_e_x(e_x, "PWD");
+//        assign_var("PWD", (char *)arg_cd, e_x);
+//        assign_var("OLDPWD", temp, e_x);
+//    }
+//    else
+//    {
+//        char *temp = find_e_x(e_x, "PWD");
+//        char *res = final_path((char *)arg_cd, current_path);
+//        assign_var("OLDPWD", temp, e_x);
+//        assign_var("PWD", res, e_x);
+//    }
+//}
 
 int my_pwd(struct exec_struct *e_x)
 {
@@ -197,21 +199,25 @@ int my_cd(char **argv, struct exec_struct *e_x)
         return 2;
     }
 
-    //gerer les chemins ABS
-    if (*arg_cd == '/')
+    if (*arg_cd == '~')
+    {
+        current_path = find_e_x(e_x, "HOME");
+        arg_cd[0] = '.';
+    }
+
+    if (*arg_cd == '/')     //gerer les chemins ABS
     {
         assign_var("OLDPWD", current_path, e_x);
         assign_var("PWD", arg_cd, e_x);
     }
     else
-        change_pwd_var2(current_path, arg_cd, e_x);
+        change_pwd_var2(current_path, arg_cd, e_x); // gere les long arg type machin/machin/../.. + met a jour PWD et OLDPWD
+
     if (chdir(find_e_x(e_x, "PWD")) == -1) // change le working dir
     {
             fprintf(stderr, "Chdir failed\n");
             return 2;
     }
-
-     // met a jour OLDPWD et PWD
 
     if (need_pwd == 1)
         printf("%s\n", current_path);
@@ -239,7 +245,7 @@ void change_pwd_var2(char *current_path, const char *arg_cd, struct exec_struct 
     char *trash = hcalloc(1, strlen(arg_cd));
     strcpy(trash, arg_cd);
     char *tmp = NULL;
-    (tmp = strtok(trash, "/"));
+    tmp = strtok(trash, "/");
     do {
         if (strcmp(tmp, ".") == 0)
             ;
